28debc37c210883dba10a3852e915325dea82abf
sanjeet0000 <sanjeetdeshwal@outlook.com>
2024-01-28 20:17:01 +0530

    minor

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,7 +1,7 @@
 import "./Footer.css";
 
 function Footer() {
-  return <footer className="footer">Sanjeet 2023</footer>;
+  return <footer className="footer">Sanjeet 2024</footer>;
 }
 
 export default Footer;
